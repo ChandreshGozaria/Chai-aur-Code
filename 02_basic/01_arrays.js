a5feb6007352cde80c77fde08dab2bcbda9a21cd
ChandreshGozaria <chandresh.gozaria@gmail.com>
2024-04-11 07:47:55 +0530

    Array

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,27 @@
+const myArr = [0, 1, 2, 3, 4, 5];
+
+console.log("A: ", myArr);
+
+// let newArr1 = myArr.join();
+// console.log(" new" ,typeof newArr1);
+
+// const newArr1 = myArr.slice(1, 3);
+// console.log("Slice: ", newArr1); 
+
+// const newArr2 = myArr.splice(1, 3, "clasic", "chandu");
+// console.log("splice: ", newArr2);
+
+const users = ["Ted", "Tim", "Ton", "Sam", "Sor", "Sod"];
+const arr = ["a", "b", "c", "d", "e"]
+// ["f", "g"]
+
+// arr.splice(2,1, "f", "g")
+console.log(arr.splice()); 
+
+// const myArr2 = users.slice(1);
+// console.log(myArr2);
+
+// users.splice(1, 3, "Hi")
+
+// console.log(users);
+console.log("Old --", arr);
---
+++ 02_basic/02_array.js
@@ -0,0 +1,25 @@
+const marvel_heros = ["ironman", "thor", "spiderman"];
+// console.log(marvel_heros);
+
+const dc_heros = ["superman", "flash", "batman"];
+
+// marvel_heros.push(dc_heros);
+// let allHeros = marvel_heros.concat(dc_heros)
+
+// let allHeros = [...marvel_heros, ...dc_heros];
+
+// console.log(allHeros);
+
+// let numArr = [1, 2, 3, [6, 2, 8], [ 7, 0, [ 2, 1, 4, 9]]];
+// console.log(numArr.flat(Infinity));
+
+// const namearr = Array.isArray("clsssic");
+// const newArr = Array.from({name: "classic"})
+
+// console.log(newArr);
+
+let num1 = 100;
+let num2 = 200;
+let num3 = 300;
+
+console.log(Array.of(num1, num2, num3));
